a687bb67e7e69fc5ae55f720f2df22b6a7f9a117
cesar gutierrez <cesargusa22@gmail.com>
2020-12-06 18:59:28 +0100

    He añadido un metodo para cuando se meta un Order Id que no exista de error y no introduzca el producto en el order id

--- src/main/java/com/example/Product/API/OrdersProductsViewController.java
+++ src/main/java/com/example/Product/API/OrdersProductsViewController.java
@@ -2,15 +2,20 @@ package com.example.Product.API;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.example.Product.Model.Order;
 import com.example.Product.Model.OrderProduct;
 import com.example.Product.Model.Product;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -41,14 +46,32 @@ public class OrdersProductsViewController {
 
     //METODO QUE CREA UN ORDERPRODUCT Y REDIRIGE A LA INFORMACION DEL PEDIDO AÑADIDO
     @PostMapping("/OrderProducts")
-    public String Add(@ModelAttribute("orderProduct") OrderProduct newOrderProduct){
-        OrdersProductsController.orderproducts.add(
-            new OrderProduct(
-            newOrderProduct.getCantidad(),
-            newOrderProduct.getOrderId(),
-            newOrderProduct.getProductId()
-            ));
-        return "redirect:/OrderProducts?orderId="+newOrderProduct.getOrderId();
+    public String Add(@ModelAttribute("orderProduct") OrderProduct newOrderProduct, HttpServletRequest request, HttpServletResponse response){
+        for(Order ele : OrdersController.orders){
+           
+                  if(ele.getId()==newOrderProduct.getOrderId()){
+                OrdersProductsController.orderproducts.add(
+                    new OrderProduct(
+                    newOrderProduct.getCantidad(),
+                    newOrderProduct.getOrderId(),
+                    newOrderProduct.getProductId()
+                    ));
+                return "redirect:/OrderProducts?orderId="+newOrderProduct.getOrderId();
+            }else{
+                throw new ElementNotFoundException();
+           /* response.setStatus(HttpServletResponse.SC_NOT_FOUND); */
+            }
+        }
+            
+          
+                    return null;
+
+      
     }
    
+    //METODO QUE MUESTRA UN ERROR SI SE INTRODUCE UNA ID INCORRECTA
+    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="----ID INCORRECTA-----")
+    public class ElementNotFoundException extends RuntimeException{
+        
+    }
 }
