5d8ce83766c826fa2a8ddbed35a322f56ad28ffc
cesar gutierrez <cesargusa22@gmail.com>
2020-12-07 01:39:49 +0100

    Creado el html de ecommerce donde muestrare la tienda por html, he añadido en el OrderProductsViewController un mapeo a Ecomerce, y saco todos los productos y arriba los nombres de los pedidos

--- src/main/java/com/example/Product/API/OrdersProductsViewController.java
+++ src/main/java/com/example/Product/API/OrdersProductsViewController.java
@@ -2,20 +2,17 @@ package com.example.Product.API;
 
 import java.util.ArrayList;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 import com.example.Product.Model.Order;
 import com.example.Product.Model.OrderProduct;
 import com.example.Product.Model.Product;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -32,7 +29,28 @@ public class OrdersProductsViewController {
         mv.addObject("ord", result2);
         mv.addObject("productsOrder", productsByOrder);
        return mv;
+
+
     }
+    @GetMapping("/Ecommerce")
+    public ModelAndView GetAll2(@RequestParam( name="orderId", required=false,defaultValue="0") int orderId){
+        ArrayList<OrderProduct> result = OrdersProductsController.GetOrderProductbyOrder(orderId);
+        ArrayList<Order> result2 = OrdersController.GetAllOrdersId(orderId);
+        ArrayList<Product> productsByOrder = OrdersProductsController.GetProductsByOrderId(orderId);
+        ArrayList<Product> pro = ProductsController.GetProductss();
+
+        ModelAndView mv = new ModelAndView("ecommerce");
+        mv.addObject("orderproduct", result);
+        mv.addObject("orders", OrdersController.orders);
+        mv.addObject("ord", result2);
+        mv.addObject("productsOrder", productsByOrder);
+        mv.addObject("pro", pro);
+       return mv;
+
+
+    }
+
+
      @GetMapping("/OrderProducts/new")
     public ModelAndView CreateOrderProduct(){
         ModelAndView mv = new ModelAndView("addProductsOrder");
@@ -46,10 +64,8 @@ public class OrdersProductsViewController {
 
     //METODO QUE CREA UN ORDERPRODUCT Y REDIRIGE A LA INFORMACION DEL PEDIDO AÑADIDO
     @PostMapping("/OrderProducts")
-    public String Add(@ModelAttribute("orderProduct") OrderProduct newOrderProduct, HttpServletRequest request, HttpServletResponse response){
-        for(Order ele : OrdersController.orders){
-           
-                  if(ele.getId()==newOrderProduct.getOrderId()){
+    public String Add(@ModelAttribute("orderProduct") OrderProduct newOrderProduct){
+  
                 OrdersProductsController.orderproducts.add(
                     new OrderProduct(
                     newOrderProduct.getCantidad(),
@@ -57,21 +73,16 @@ public class OrdersProductsViewController {
                     newOrderProduct.getProductId()
                     ));
                 return "redirect:/OrderProducts?orderId="+newOrderProduct.getOrderId();
-            }else{
-                throw new ElementNotFoundException();
+           
+                
            /* response.setStatus(HttpServletResponse.SC_NOT_FOUND); */
-            }
+            
+           
+
         }
             
-          
-                    return null;
-
+         
       
     }
-   
-    //METODO QUE MUESTRA UN ERROR SI SE INTRODUCE UNA ID INCORRECTA
-    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="----ID INCORRECTA-----")
-    public class ElementNotFoundException extends RuntimeException{
-        
-    }
-}
+
+
